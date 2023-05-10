9f1484e68f3fea808bb54c44cb35a38e144c8077
Anshul-eng <105932043+Anshul-eng@users.noreply.github.com>
2023-05-10 19:33:34 +0530

    Add files via upload

---
+++ Database - H2 and Mysql/src/main/java/com/example/db/db/model/entities/Student.java
@@ -0,0 +1,32 @@
+package com.example.db.db.model.entities;
+
+import lombok.*;
+
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Student implements Serializable {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id = 0;
+    private String name;
+    private Integer age;
+
+
+}
